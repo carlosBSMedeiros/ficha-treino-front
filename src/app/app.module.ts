c1cc9b9588a8312d1ccd51b2e14855ea1e234c8a
carlosBSMedeiros <carlos.bs.medeiros@gmail.com>
2023-03-15 16:15:12 -0300

    cria cronômetro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { FichaComponent } from './components/ficha/ficha.component';
 import { TreinoComponent } from './components/treino/treino.component';
 import { FichasComponent } from './pages/fichas/fichas.component';
 import { RelogioComponent } from './pages/relogio/relogio.component';
 import { CronometroComponent } from './components/cronometro/cronometro.component';
-
+ 
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/components/cronometro/cronometro.component.ts
+++ src/app/components/cronometro/cronometro.component.ts
@@ -7,4 +7,56 @@ import { Component } from '@angular/core';
 })
 export class CronometroComponent {
 
+  cronometroIniciado : boolean = false;
+  tempoTotal: number = 0;
+  minutos: number = 0;
+  segundos: number = 0;
+  intervalo: any;
+
+  constructor(){
+    this.cronometroIniciado = false;
+  }
+
+  iniciarPararCronometro() {
+    if(this.cronometroIniciado){
+      console.log('Cronometro parado')
+      this.pausarCronometro();
+    } else{
+      console.log('Cronometro iniciado')
+      this.iniciarCronometro();
+    }
+    this.cronometroIniciado = !this.cronometroIniciado;
+  }
+
+  iniciarCronometro(){
+    this.intervalo = setInterval(()=>
+      this.somarTempo()
+    , 1000);
+  }
+
+  somarTempo(){
+    this.tempoTotal++;
+    this.minutos = Math.floor(this.tempoTotal / 60);
+    this.segundos = this.tempoTotal % 60;
+  }
+
+  pausarCronometro(){
+    clearInterval(this.intervalo);
+  }
+
+  zerarCronometro(){
+    this.tempoTotal = 0;
+    this.minutos = 0;
+    this.segundos = 0;
+    this.cronometroIniciado = false;
+    clearInterval(this.intervalo);
+  }
+
+  formatarMinutos():string{
+    return this.minutos < 10 ? "0"+this.minutos : this.minutos.toString() 
+  }
+
+  formatarSegundos(){
+    return this.segundos < 10 ? "0"+this.segundos : this.segundos.toString() 
+  }
 }
